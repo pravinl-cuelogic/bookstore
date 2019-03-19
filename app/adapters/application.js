5e414ead491de3d7ae1c350741e957db8419e8c2
Pravin Lokhande <pravin.lokhande@cuelogic.com>
2019-03-19 19:59:34 +0530

    api fetching from rails app done.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,7 @@ import DS from 'ember-data';
 const { String: { pluralize, underscore } } = Ember;
 
 export default DS.JSONAPIAdapter.extend({
+  host: 'http://localhost:3000',
   pathForType(type){
   	return pluralize(underscore(type));
   }
--- app/routes/books.js
+++ app/routes/books.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	model() {
-		return this.store.finallAll('book');
+		return this.store.findAll('book');
 	}
 });
